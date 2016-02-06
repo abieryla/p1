d9ab48ea718e9661cdc4367ecffc0212b20b24c5
Allyson Bieryla <abieryla@cfa.harvard.edu>
2016-02-06 16:13:07 -0500

    Added README, index.php with title and photo and image subdirectory

---
+++ README.md
@@ -0,0 +1,7 @@
+Allyson Bieryla 
+================
+dwa15 Spring 2016 
+---------------
+### p1 intro page
+
+This is p1.
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="dwa15.css">
+</head>
+
+<h1>Dynamic Web Applications - Spring 2016</h1>
+<h2> Allyson Bieryla</h2>
+
+<body>
+<p class="image">
+<img src="images/abieryla.jpg">
+</p>
+</body>
+
+</html>
